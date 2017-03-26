ff948b4f99bac05b89f2c62a7483eee9b586bb61
quinnlangille <quinn@picturesandwhatever.com>
2017-03-26 14:49:34 -0400

    fixed JS wow

--- mySquad/js/selectFx.js
+++ mySquad/js/selectFx.js
@@ -140,6 +140,7 @@
 		this.selEl.className = this.el.className;
 		this.selEl.tabIndex = this.el.tabIndex;
 		this.selEl.innerHTML = '<span class="cs-placeholder">' + this.selectedOpt.textContent + '</span>' + opts_el;
+
 		this.el.parentNode.appendChild( this.selEl );
 		this.selEl.appendChild( this.el );
 	}
